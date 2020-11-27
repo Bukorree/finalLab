824825beb5dc6b388bdc032f0a83f48b2943ce98
Bukorree <73693612+Bukorree@users.noreply.github.com>
2020-11-27 17:05:55 +0700

    Update show.php

--- show.php
+++ show.php
@@ -75,7 +75,7 @@ $res = mysqli_query($conn, 'SELECT * FROM bmiDB');
                     <input type="hidden" class="form-control" name="He" value="<?php echo $Result['height']; ?>">
                  </div>
                 <div class="display-5 form-group">
-                    <input type="hidden" class="form-control" name="He" value="<?php echo $Result['id']; ?>">
+                    <input type="hidden" class="form-control" name="ide" value="<?php echo $Result['id']; ?>">
                  </div>
                 <button type="submit" class="btn btn-primary">Submit</button>
          </form>
